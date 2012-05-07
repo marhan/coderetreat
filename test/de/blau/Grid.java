35843552d39a3fe87e7822f22cb0ee3d6bb21f25
Markus Hanses <markus@markushanses.de>
2012-05-07 20:35:18 +0200

    bla

--- test/de/blau/Grid.java
+++ test/de/blau/Grid.java
@@ -5,7 +5,11 @@ public class Grid {
 
 	private final Integer[] array = new Integer[16];
 
-	public Grid() {}
+	public Grid() {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Integer(0);
+		}
+	}
 
 	public int getSize() {
 		return array.length;

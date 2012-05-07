717ec6faf0e24342f6a1983c25bbd0bc4861c262
Markus Hanses <markus@markushanses.de>
2012-05-07 18:58:07 +0200

    first class

---
+++ src/de/blau/TicTacToe.java
@@ -0,0 +1,5 @@
+package de.blau;
+
+public class TicTacToe {
+
+}
---
+++ test/de/blau/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package de.blau;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

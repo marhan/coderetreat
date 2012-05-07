2de9819b522011c37b06c602ad5bf76662b11126
Markus Hanses <markus@markushanses.de>
2012-05-07 19:40:21 +0200

    bla

--- test/de/blau/Grid.java
+++ test/de/blau/Grid.java
@@ -4,6 +4,8 @@ public class Grid {
 
 	private final int size;
 
+	private final Integer[] array = new Integer[16];
+
 	public Grid(int i) {
 		// TODO Auto-generated constructor stub
 		size = i;
--- test/de/blau/TicTacToeTest.java
+++ test/de/blau/TicTacToeTest.java
@@ -41,4 +41,5 @@ public class TicTacToeTest {
 		assertNotNull(ticTacToe.getGrid());
 	}
 
+
 }
